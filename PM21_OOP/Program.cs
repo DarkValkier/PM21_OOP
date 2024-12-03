148417effba6d6750ba89dbeb6f9a0ebf7a6efcd
DarkValkier <darkvalkier@gmail.com>
2024-12-03 14:48:15 +0200

    Добавьте файлы проекта.

---
+++ PM21_OOP/Program.cs
@@ -0,0 +1,17 @@
+﻿using character;
+
+Character player1 = new Character("Vasya", 100, 3, 0);
+Character player2 = new Character("Petya", 85, 5, 0);
+
+player1.showStats();
+Console.WriteLine();
+player2.showStats();
+Console.WriteLine();
+
+player1.attack(player2);
+player2.attack(player1);
+
+Console.WriteLine("After attack:");
+player1.showStats();
+Console.WriteLine();
+player2.showStats();
---
+++ PM21_OOP/character.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace character
+{
+    class Character
+    {
+        private string name;
+        private int health;
+        private int damage;
+        private int defence;
+
+        public Character(string name, int health, int damage, int defence)
+        {
+            this.name = name;
+            this.health = health;
+            this.damage = damage;
+            this.defence = defence;
+        }
+
+        public Character()
+        {
+            this.name = "Unknown";
+            this.health = 100;
+            this.damage = 1;
+            this.defence = 0;
+        }
+
+        public void showStats()
+        {
+            Console.WriteLine($"Name: {this.name}");
+            Console.WriteLine($"Health: {this.health}");
+            Console.WriteLine($"Damage: {this.damage}");
+            Console.WriteLine($"Defence: {this.defence}");
+        }
+
+        public void takeDamage( int damage )
+        {
+            this.health = Math.Max(this.health - Math.Max(damage, 0), 0);
+        }
+
+        public void attack(Character target)
+        {
+            target.takeDamage(this.damage);
+        }
+    }
+}
